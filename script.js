8e7b293f62b37fefd91672fe19148de218ee46b1
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2025-07-08 12:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+const button=document.getElementById("button");
+const urlDisplay=document.getElementById("https://localhost:8080");
+
+button.addEventListener("click",function () {
+   const name = document.getElementById("name").value.trim();
+      const year = document.getElementById("year").value.trim();
+
+      let baseUrl = "https://localhost:8080/";
+      let params = [];
+
+      if (name) {
+        params.push("name=" + encodeURIComponent(name));
+      }
+
+      if (year) {
+        params.push("year=" + encodeURIComponent(year));
+      }
+
+      const queryString = params.length ? "?" + params.join("&") : "";
+
+      urlDisplay.textContent = baseUrl + queryString;	
+});
